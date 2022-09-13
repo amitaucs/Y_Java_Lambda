438989bc910d5aa03f4a51c1663a63f2a0dfdd6b
amitdatta <the.amisoft@gmail.com>
2022-09-13 11:59:21 +0530

    map example

---
+++ src/main/java/org/codetechies/StreamMapExample.java
@@ -0,0 +1,18 @@
+package org.codetechies;
+
+import org.codetechies.data.Employee;
+import org.codetechies.utils.EmployeeDataUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamMapExample {
+
+    public static void main(String[] args) {
+        List<Employee> empList = EmployeeDataUtils.getAllEmployee();
+        List<String> empNameList = empList.stream().map(Employee::getName).collect(Collectors.toList());
+
+        empNameList.forEach(empName -> System.out.println(empName));
+    }
+
+}
